41731fb378e3999eba6a4fcc681fce25c1c9cac7
Daniel P. Egan <dp.egan@gmail.com>
2017-03-19 12:19:53 -0400

    good stuff. Polishing.

--- blog/visualizing_risk/distribution_plots_of_returns.R
+++ blog/visualizing_risk/distribution_plots_of_returns.R
@@ -1,7 +1,16 @@
 
+# A very simply way to get fixed-Sharpe portfolios stats based on risk level. 
+# Anchored to a 100% stock portfolio with 6% return and 17% vol. 
+portstats <- function(risk_level){
+  er <- 6 * risk_level
+  sd <- 17 * risk_level
+  return(c(er = er, sd = sd))
+}
 
+
+# A workhorse function to produce the results. 
 cumulative_results <- function(risk_level, ptiles = c(0.20, 0.5, 0.80) ){
-  ps <- portstats_v2(risk_level)
+  ps <- portstats(risk_level)
   raw_returns <- matrix(rnorm(n = 100000, mean = ps['er'], sd = ps['sd']) / 100, 
                         nrow=10)
   returns_cumul <- apply(raw_returns, 2, function(x) {cumprod(x+1)}) - 1
