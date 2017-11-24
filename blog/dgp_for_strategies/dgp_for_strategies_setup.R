e9faca12e85f6516a38bc472b292a859110cadde
Daniel P. Egan <dp.egan@gmail.com>
2017-11-24 17:19:17 -0500

    expansion of analysis, regularization.

--- blog/dgp_for_strategies/dgp_for_strategies_setup.R
+++ blog/dgp_for_strategies/dgp_for_strategies_setup.R
@@ -4,22 +4,28 @@ pacman::p_load("markovchain", ggplot2, scales, reshape2, dplyr, rowr, zoo)
 
 # Define the data-generating process ----
 years <- 10
-sims <- 1000
 nobs <- years * 252
+
+sims <- 1000
+sim_vector <- 1:sims          
+set.seed(8675309) 
+
 good_er <- 4
 good_vol <- 12
 bad_er <- 0
 bad_vol <- 20
 
 market_states <- c("good", "bad")
-good_to_bad_transition_prob <- 0.25
-bad_to_good_transition_prob <- 0.35
+good_to_bad_transition_prob <- 0.15
+bad_to_good_transition_prob <- 0.25
 
 window <- 22
 threshold_p <- 0.02 # 2% annual return
-set.seed(8675309) 
 
 
+
+
+# Set up the underlying DGP returns
 good_market_returns <- matrix(rnorm(nobs * sims, mean = good_er, sd=good_vol), ncol = sims, nrow=nobs) 
 good_market_returns_daily <- (1 + good_market_returns/100) ^ (1/252)
 bad_market_returns <- matrix(rnorm(nobs * sims, mean = bad_er, sd=bad_vol), ncol = sims, nrow=nobs)
@@ -31,13 +37,11 @@ market_transition_matrix <- matrix(data = c((1-good_to_bad_transition_prob), goo
                                             byrow = byRow, nrow = 2, dimnames = list(market_states, market_states))
 
 mc_market <- new("markovchain", states = market_states, byrow = byRow, transitionMatrix = market_transition_matrix, name = "market")
-
-
-# Create a set of sample market outcomes based on your DGP
-sim_vector <- 1:sims                              
 mc_market_sim_states <- as.matrix(sapply(sim_vector, function(x) rmarkovchain(n = nobs , object = mc_market, t0 = "good")), 
                                   nrow=nobs,ncol=sims)
 
+
+# Generate the Assumed World:  regime switching. 
 mc_market_sim_returns <- matrix(NA, nrow = nrow(mc_market_sim_states), ncol = ncol(mc_market_sim_states))
 
 good_index <- which(mc_market_sim_states == "good")
@@ -49,75 +53,106 @@ mc_market_sim_returns[1, ] <- 1
 
 mc_market_sim_returns_decimal <- mc_market_sim_returns - 1
 
-# Generate a similar world, but without regime switching. This will be the 'null world'.
+# Generate the Null World, just random, no regime switching. 
+# Notes: using the exact same mean outcomes as the hypothesized world. 
+# It's about timing, no average market returns.
 
 empirical_mean <- mean(mc_market_sim_returns_decimal)
 empirical_sd   <- sd(mc_market_sim_returns_decimal)
+mc_market_sim_returns_null_decimal <- matrix(rnorm(n = sims * nobs, 
+                                                   mean = empirical_mean*100, 
+                                                   sd = empirical_sd*100), 
+                                     nrow = nrow(mc_market_sim_states), 
+                                     ncol = ncol(mc_market_sim_states)) / 100
+mc_market_sim_returns_null_decimal[1, ] <- 1
 
-
-mc_market_sim_returns_null_decimal <- matrix(rnorm(n = sims * nobs, mean = empirical_mean*100, sd = empirical_sd*100), 
-                                     nrow = nrow(mc_market_sim_states), ncol = ncol(mc_market_sim_states)) / 100
-mc_market_sim_returns_null_decimal[1:10, 1:10]
-# Cumulative returns
 mc_market_sim_returns_cumul <- apply(mc_market_sim_returns, 2, function(x) cumprod(x))
 mc_market_sim_returns_null_cumul <- apply(mc_market_sim_returns_null_decimal, 2, function(x) cumprod(x+1))
-
 mc_market_sim_returns_df_long <- melt(mc_market_sim_returns_cumul)
+names(mc_market_sim_returns_df_long) <- c("day", "scenario","value")
 mc_market_sim_returns_null_df_long <- melt(mc_market_sim_returns_null_cumul)
+names(mc_market_sim_returns_null_df_long) <- c("day", "scenario","value")
 
-
-# Define a strategy that should work in that process
-
-strategy_market_timing <- function(return_data, threshold, weighted = FALSE){
-  if(weighted){
-    weights <- 1/seq(1:length(return_data))
-    weights <- weights/sum(weights)
-    measure <- weighted.mean(return_data, weights)
-  } else {
-    measure <- mean(return_data)
-  }
-  signal <- ifelse(measure >= threshold, 1,0)
-  return(signal)
-}
-threshold <- ((1 + threshold_p) ^ (1/252)) - 1
-# strategy_market_timing(rep(1, 30)) 
-# strategy_market_timing(rep(2, 30)) 
-# strategy_market_timing(rep(2.1, 30)) 
-# strategy_market_timing(seq(0, 3.2, length.out = 30)) 
-# strategy_market_timing(mean(seq(3.2,0, length.out = 30)),  threshold =  2) 
-# strategy_market_timing(mc_market_sim_returns_decimal[22:44,1], threshold = threshold) 
-# mean(mc_market_sim_returns_decimal[22:44,2]) > threshold
 
 # Run the strategy through the DGP, compare it to a benchmark
-signals <- matrix(NA, nrow = nobs, ncol = sims)
-mc_market_sim_returns_decimal <- zoo(mc_market_sim_returns_decimal, 
-                                     seq.Date(Sys.Date(), (Sys.Date()+nobs-1), by=1))
+calcStrategySignalAndReturns <- function(return_matrix, annual_threshold, window){
+  nobs = nrow(return_matrix)
+  sims = ncol(return_matrix)
+  daily_threshold <- (1 +annual_threshold) ^ (1/252) - 1
+  signals <- matrix(NA, nrow = nobs, ncol = sims)
+  return_matrix_zoo <- zoo(return_matrix, 
+                           seq.Date(Sys.Date(), (Sys.Date()+nobs-1), by=1)) #dates don't really matter.
+  
+  # This is the strategy implementation, faster than writing an actual function. 
+  # Create a rolling mean. 
+  mean_returns <- rollmean(x= return_matrix_zoo, 
+                           k = window,
+                           align = "right", fill = NA)
+  
+  signals[which(mean_returns >= daily_threshold)] <- 1
+  signals[which(mean_returns < daily_threshold)] <- 0
+  signals[1:window,] <- 1 # During the burn-in period, buy.
+  
+  # Create an index of when the strategy would have bought.
+  buy_index <- which(signals == 1)
+  
+  port_return_matrix <- matrix(0, ncol=ncol(return_matrix), nrow=nrow(return_matrix))
+  port_return_matrix <- unname(as.matrix(port_return_matrix)) # remove zoo stuff
+  
+  port_return_matrix[buy_index] <- mc_market_sim_returns_decimal[buy_index]
+  port_return_matrix_cumul <- apply(unname(port_return_matrix),       2, function(x) cumprod(x+1))
+  
+  port_return_matrix_cumul_long <- melt(port_return_matrix_cumul)
+  names(port_return_matrix_cumul_long) <- c("day","scenario","value")
+  
+  return(list(signals = signals, 
+              buy_index = buy_index, 
+              port_return_matrix = port_return_matrix, 
+              port_return_matrix_cumul = port_return_matrix_cumul, 
+              port_return_matrix_cumul_long = port_return_matrix_cumul_long))
+}
 
-mean_returns <- rollmean(x= mc_market_sim_returns_decimal, 
-                         k = window,
-                         align = "right", fill = NA)
+# Run the strategy through both the null and believed world view.
+res_market_null_strategy_true <- calcStrategySignalAndReturns(mc_market_sim_returns_null_decimal, 
+                                                              annual_threshold  = threshold_p, 
+                                                              window = window)
 
-signals[which(mean_returns >= threshold)] <- 1
-signals[which(mean_returns < threshold)] <- 0
-signals[1:window,] <- 1
+res_market_true_strategy_true <- calcStrategySignalAndReturns(mc_market_sim_returns_decimal,
+                                                              annual_threshold = threshold_p, 
+                                                              window = window)
 
-# See how good the strategy is across futures. 
-buy_index <- which(signals == 1)
 
-mc_strategy_sim_returns <- matrix(0, ncol=ncol(mc_market_sim_returns_decimal), nrow=nrow(mc_market_sim_returns_decimal))
-mc_market_sim_returns_decimal <- unname(as.matrix(mc_market_sim_returns_decimal)) # remove zoo stuff
+# Conversely, for the strategy = FALSE, we just create buy-and-hold metrics.
+calcNullStrategyReturns <- function(return_matrix){
+  
+  port_return_matrix_cumul <- apply(unname(return_matrix),       2, function(x) cumprod(x+1))
+  
+  port_return_matrix_cumul_long <- melt(port_return_matrix_cumul)
+  names(port_return_matrix_cumul_long) <- c("day","scenario","value")
+  
+  return(list(port_return_matrix = return_matrix, 
+              port_return_matrix_cumul = port_return_matrix_cumul, 
+              port_return_matrix_cumul_long = port_return_matrix_cumul_long))
+}
 
-mc_strategy_sim_returns[buy_index] <- mc_market_sim_returns_decimal[buy_index]
+res_market_true_strategy_null <- calcNullStrategyReturns(mc_market_sim_returns_decimal)
+res_market_null_strategy_null <- calcNullStrategyReturns(mc_market_sim_returns_null_decimal)
 
-strategy_cumul_values <- apply(unname(mc_strategy_sim_returns),       2, function(x) cumprod(x+1))
-passive_cumul_values <-  apply(unname(mc_market_sim_returns_decimal), 2, function(x) cumprod(x+1))
+res_market_true_strategy_null$port_return_matrix_cumul_long$strategy <- "passive"
+res_market_null_strategy_null$port_return_matrix_cumul_long$strategy <- "passive"
+res_market_true_strategy_true$port_return_matrix_cumul_long$strategy <- "active"
+res_market_null_strategy_true$port_return_matrix_cumul_long$strategy <- "active"
 
-strategy_cumul_values_long <- melt(strategy_cumul_values)
-strategy_cumul_values_long$strategy <- "active"
+res_market_true_strategy_null$port_return_matrix_cumul_long$world <- "regime"
+res_market_null_strategy_null$port_return_matrix_cumul_long$world <- "null"
+res_market_true_strategy_true$port_return_matrix_cumul_long$world <- "regime"
+res_market_null_strategy_true$port_return_matrix_cumul_long$world <- "null"
 
-passive_cumul_values_long <- melt(passive_cumul_values)
-passive_cumul_values_long$strategy <- "passive"
+cumul_values_all <- rbind(res_market_true_strategy_true$port_return_matrix_cumul_long, 
+                          res_market_null_strategy_true$port_return_matrix_cumul_long,
+                          res_market_true_strategy_null$port_return_matrix_cumul_long, 
+                          res_market_null_strategy_null$port_return_matrix_cumul_long)
 
-cumul_values_all <- rbind(passive_cumul_values_long, strategy_cumul_values_long)
 
 save.image("dgp_for_strategies.RData")
+
