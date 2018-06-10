b25aa9d799ecdf7993554ecf517aec8d2f04d71a
Daniel P. Egan <dp.egan@gmail.com>
2018-06-10 19:24:50 -0400

    improvements

--- degan_tweets/run_crontwit.R
+++ degan_tweets/run_crontwit.R
@@ -11,7 +11,6 @@ if(!require(crontwit)){
 if(as.numeric(format(Sys.time(), "%H")) == 1){
   devtools::install_github("bestdan/crontwit")
 }
-
 library(crontwit)
 
 
@@ -23,7 +22,8 @@ tweet_db_path <- "~/src/myscellany/degan_tweets/tweet_db.rda"
 twitter_creds <- yaml.load_file(creds_path)$twitter
 
 # Tell it to auto-cache the credentials
-getOption("httr_oauth_cache")
+options("httr_oauth_cache"=TRUE)
+#getOption("httr_oauth_cache")
 setup_twitter_oauth(consumer_key = twitter_creds$consumer_key, 
                     consumer_secret = twitter_creds$consumer_secret, 
                     access_token = twitter_creds$access_token, 
@@ -34,4 +34,4 @@ load(schedule_path)
 load(tweet_db_path)
 
 # Check schedule and post if necessary. 
-checkScheduleAndPost(schedule, tweet_db)
+checkScheduleAndPost(schedule, tweet_db)\
