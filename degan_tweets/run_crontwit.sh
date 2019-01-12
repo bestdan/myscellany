0e5e6147c9f82fd652a8cc4ee8d69ca668a31e83
Daniel P. Egan <dp.egan@gmail.com>
2019-01-12 10:58:40 -0500

    deletes

--- degan_tweets/run_crontwit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-{
-  sudo Rscript src/myscellany/degan_tweets/run_contwit.R
-} >> src/myscellany/degan_tweets/log/bash_log.txt
--- degan_tweets/update_repo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo git pull origin master
